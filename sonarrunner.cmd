26154c1a60a11738b3ec123490690219cc50846f
Zoran Vukoszavlyev <vzoran76@gmail.com>
2016-05-17 23:58:54 +0200

    Setup jenkins to measure coverage

--- sonarrunner.cmd
+++ sonarrunner.cmd
@@ -1,5 +1,4 @@
-MSBuild.SonarQube.Runner.exe begin /k:"EA2G" /n:"EA2Gliffy" /v:"0.1" /d:sonar.cs.vscoveragexml.reportsPaths="%CD%\opencover.xml"
+MSBuild.SonarQube.Runner.exe begin /k:"EA2G" /n:"EA2Gliffy" /v:"0.1" /d:sonar.cs.opencover.reportsPaths="%CD%\opencover.xml"
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Rebuild
-rem "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" /testcontainer:"eatogliffyTest\bin\Debug\eatogliffyTest.dll" /resultsfile:MSTestResults.trx
 "%LOCALAPPDATA%\Apps\OpenCover\OpenCover.Console.exe" -output:"%CD%\opencover.xml" -register:user -target:"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" -targetargs:"eatogliffyTest\bin\Debug\eatogliffyTest.dll"
 MSBuild.SonarQube.Runner.exe end
