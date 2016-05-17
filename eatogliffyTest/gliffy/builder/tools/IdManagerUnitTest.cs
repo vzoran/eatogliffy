097179a29fa391b0520a251de5ad9cade3859822
Zoran Vukoszavlyev <vzoran76@gmail.com>
2016-05-17 23:58:34 +0200

    Improve unit testing

--- eatogliffyTest/gliffy/builder/tools/IdManagerUnitTest.cs
+++ eatogliffyTest/gliffy/builder/tools/IdManagerUnitTest.cs
@@ -7,11 +7,12 @@ namespace eatogliffyTest.gliffy.builder.tools
     [TestClass]
     public class IdManagerUnitTest
     {
+        private const string key01 = "key01";
+        private const string key02 = "key02";
+
         [TestMethod]
         public void TestGetIdWithKey()
         {
-            const string key01 = "key01";
-            const string key02 = "key02";
             IdManager.Reset();
 
             int key01id = IdManager.GetId(key01);
@@ -20,6 +21,20 @@ namespace eatogliffyTest.gliffy.builder.tools
             Assert.AreEqual(0, key01id, 0f, "First index must be 0");
             Assert.AreEqual(1, key02id, 0f, "Second index must be diffrerent than 0");
             Assert.AreEqual(0, key01id, 0f, String.Format("index of {0} must be 0", key01id));
+            Assert.AreEqual(1, key02id, 0f, "Second index must be diffrerent than 0");
+        }
+
+        [TestMethod]
+        public void TestReset()
+        {
+            IdManager.Reset();
+
+            IdManager.GetId(key01);
+            IdManager.GetId(key02);
+            Assert.AreNotEqual(0, IdManager.Counter, 0f, "Last index of IDManager cannot be 0");
+
+            IdManager.Reset();
+            Assert.AreEqual(0, IdManager.Counter, 0f, "Last index of IDManager must be 0 after reset");
         }
     }
 }
---
+++ eatogliffyTest/gliffy/io/EaObjectUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using eatogliffy.gliffy.io;
+
+namespace eatogliffyTest.gliffy.io
+{
+    [TestClass]
+    public class EaObjectUnitTest
+    {
+        [TestMethod]
+        public void TestConstrutors()
+        {
+            string id = "001";
+            string name = "name01";
+            string parentId = "002";
+            EaObject eaObject01 = new EaObject(id, name, true);
+            Assert.AreEqual(id, eaObject01.Id, String.Format("ID must be {0}", id));
+            Assert.AreEqual(name, eaObject01.Name, String.Format("Name must be {0}", name));
+            Assert.AreEqual(true, eaObject01.IsDiagram, String.Format("IsDIagram must be {0}", true));
+
+            EaObject eaObject02 = new EaObject(id, name, parentId, true);
+            Assert.AreEqual(id, eaObject02.Id, String.Format("ID must be {0}", id));
+            Assert.AreEqual(name, eaObject02.Name, String.Format("Name must be {0}", name));
+            Assert.AreEqual(true, eaObject02.IsDiagram, String.Format("IsDIagram must be {0}", true));
+            Assert.AreEqual(parentId, eaObject02.ParentId, String.Format("ParentId must be {0}", parentId));
+        }
+    }
+}
