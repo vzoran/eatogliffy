e0960e05be5ef2789367a97a500bb91ef97f195e
Zoran Vukoszavlyev <vzoran76@gmail.com>
2018-06-30 10:50:42 +0200

    Add DocWriter

---
+++ mddocgen/IO/IDocWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MdDocGenerator.IO
+{
+    public interface IDocWriter
+    {
+        void WritePackageContent(long itemId, string itemName, string itemString);
+        void WriteImageReference(long itemId, string itemName);
+        string GetImageFolder(long itemId, string itemName);
+
+    }
+}
